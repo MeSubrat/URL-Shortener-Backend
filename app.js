0add1f48bab06b856da06b633d5b8d3ea3aafea4
Subrat Kumar Sahoo <subratksahoo86@gmail.com>
2026-01-09 20:30:25 +0530

    Deploy ready

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 corsOptions = {
     origin: 'http://localhost:5173',
     optionsSuccessStatus: 200,
